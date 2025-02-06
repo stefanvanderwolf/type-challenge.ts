51c0e1245de41b5b093d64017c9fefd26bd36b6b
Stefan van der Wolf <stefan.van.der.wolf@persgroep.net>
2025-02-06 22:54:28 +0100

    Complete readonly

---
+++ src/easy/readonly.ts
@@ -0,0 +1,18 @@
+import type { Equal, Expect } from '@type-challenges/utils'
+
+export type cases = [
+  Expect<Equal<MyReadonly<Todo1>, Readonly<Todo1>>>,
+]
+
+interface Todo1 {
+  title: string
+  description: string
+  completed: boolean
+  meta: {
+    author: string
+  }
+}
+
+export type MyReadonly<T> = {
+  readonly [P in keyof T]: T[P]
+}

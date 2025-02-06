6e91d8ebab7656db09909d8bc258d520d918ab0a
Stefan van der Wolf <stefan.van.der.wolf@persgroep.net>
2025-02-06 12:50:06 +0100

    Complete return-type

---
+++ src/medium/return-type.ts
@@ -0,0 +1,27 @@
+import type { Equal, Expect } from '@type-challenges/utils'
+
+export type cases = [
+  Expect<Equal<string, MyReturnType<() => string>>>,
+  Expect<Equal<123, MyReturnType<() => 123>>>,
+  Expect<Equal<ComplexObject, MyReturnType<() => ComplexObject>>>,
+  Expect<Equal<Promise<boolean>, MyReturnType<() => Promise<boolean>>>>,
+  Expect<Equal<() => 'foo', MyReturnType<() => () => 'foo'>>>,
+  Expect<Equal<1 | 2, MyReturnType<typeof fn>>>,
+  Expect<Equal<1 | 2, MyReturnType<typeof fn1>>>,
+]
+
+type ComplexObject = {
+  a: [12, 'foo']
+  bar: 'hello'
+  prev(): number
+}
+
+const fn = (v: boolean) => v ? 1 : 2
+const fn1 = (v: boolean, _w: any) => v ? 1 : 2
+
+type MyReturnType<T> = 
+  // Eat up any arguments (or nothing) since we only care about the
+  // return type.
+  T extends (...args: any) => infer R 
+    ? R 
+    : never

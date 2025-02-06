213605a94fbbedcdd9c464754ac063e7c5edb4d8
Stefan van der Wolf <stefan.van.der.wolf@persgroep.net>
2025-02-06 22:00:50 +0100

    Complete exclude

---
+++ src/easy/exclude.ts
@@ -0,0 +1,9 @@
+import type { Equal, Expect } from '@type-challenges/utils'
+
+export type cases = [
+  Expect<Equal<MyExclude<'a' | 'b' | 'c', 'a'>, 'b' | 'c'>>,
+  Expect<Equal<MyExclude<'a' | 'b' | 'c', 'a' | 'b'>, 'c'>>,
+  Expect<Equal<MyExclude<string | number | (() => void), Function>, string | number>>,
+]
+
+export type MyExclude<T, U> = T extends U ? never : T;

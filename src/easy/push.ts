234a6760a9f4b8bf6f35d3d9208a3bf001c9bd9b
Stefan van der Wolf <stefan.van.der.wolf@persgroep.net>
2025-02-08 22:04:51 +0100

    Complete push

---
+++ src/easy/push.ts
@@ -0,0 +1,9 @@
+import type { Equal, Expect } from '@type-challenges/utils'
+
+export type cases = [
+  Expect<Equal<Push<[], 1>, [1]>>,
+  Expect<Equal<Push<[1, 2], '3'>, [1, 2, '3']>>,
+  Expect<Equal<Push<['1', 2, '3'], boolean>, ['1', 2, '3', boolean]>>,
+]
+
+export type Push<T extends unknown[], U> = [...T, U];

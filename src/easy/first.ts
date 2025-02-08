b943e46b46921619df34517893adb9180dd56930
Stefan van der Wolf <stefan.van.der.wolf@persgroep.net>
2025-02-08 22:23:27 +0100

    Complete first

---
+++ src/easy/first.ts
@@ -0,0 +1,20 @@
+import type { Equal, Expect } from '@type-challenges/utils'
+
+export type cases = [
+  Expect<Equal<First<[3, 2, 1]>, 3>>,
+  Expect<Equal<First<[() => 123, { a: string }]>, () => 123>>,
+  Expect<Equal<First<[]>, never>>,
+  Expect<Equal<First<[undefined]>, undefined>>,
+]
+
+type errors = [
+  // @ts-expect-error
+  First<'notArray'>,
+  // @ts-expect-error
+  First<{ 0: 'arrayLike' }>,
+]
+
+export type First<T extends unknown[]> =
+  T extends [infer F, ...infer _]
+    ? F
+    : never;

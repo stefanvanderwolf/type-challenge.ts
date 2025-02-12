011bbbaa0ca57a1a9cff44fe63ffee671226a683
Stefan van der Wolf <stefan.van.der.wolf@persgroep.net>
2025-02-12 22:14:10 +0100

    Complete last

---
+++ src/medium/last.ts
@@ -0,0 +1,13 @@
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Last<[]>, never>>,
+  Expect<Equal<Last<[2]>, 2>>,
+  Expect<Equal<Last<[3, 2, 1]>, 1>>,
+  Expect<Equal<Last<[() => 123, { a: string }]>, { a: string }>>,
+]
+
+export type Last<T extends unknown[]> =
+  T extends [...unknown[], infer Element]
+    ? Element
+    : never

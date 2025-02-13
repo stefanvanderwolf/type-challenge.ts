397f64894ec6ee39624de97baa5bd5a0ef5991fb
Stefan van der Wolf <stefan.van.der.wolf@persgroep.net>
2025-02-13 23:32:57 +0100

    Complete replace-first

---
+++ src/medium/replace-first.ts
@@ -0,0 +1,17 @@
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<ReplaceFirst<[1, 2, 3], 3, 4>, [1, 2, 4]>>,
+  Expect<Equal<ReplaceFirst<['A', 'B', 'C'], 'C', 'D'>, ['A', 'B', 'D']>>,
+  Expect<Equal<ReplaceFirst<[true, true, true], true, false>, [false, true, true]>>,
+  Expect<Equal<ReplaceFirst<[string, boolean, number], boolean, string>, [string, string, number]>>,
+  Expect<Equal<ReplaceFirst<[1, 'two', 3], string, 2>, [1, 2, 3]>>,
+  Expect<Equal<ReplaceFirst<['six', 'eight', 'ten'], 'eleven', 'twelve'>, ['six', 'eight', 'ten']>>,
+]
+
+type ReplaceFirst<List extends readonly unknown[], Replacee, Replacement> =
+  List extends [infer Head, ...infer Tail]
+    ? Head extends Replacee
+      ? [Replacement, ...Tail]
+      : [Head, ...ReplaceFirst<Tail, Replacee, Replacement>]
+    : []  
